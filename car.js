263b8ddb1b48af9db0481ffc572c36d781075f39
AlyonaGladych <41871016+AlyonaGladych@users.noreply.github.com>
2018-08-08 21:00:11 +0300

    Create car.js

---
+++ car.js
@@ -0,0 +1,26 @@
+let Vehicle = require('./vehicle.js');
+
+class Car extends Vehicle {
+    constructor (carModel, carYear, carSpeed) {
+        super(carModel, carYear, carSpeed);
+    }
+    displayInfo() {
+        console.log(`It's a car model ${this.model};
+        made in year ${this.year};
+        car max speed ${this.carSpeed}`);
+    }
+
+    advertise() {
+        console.log("Brand new car for the best price ever. Buy right now or someone else does");
+    }
+    
+    transportPeople() {
+        console.log("I'm starting to transport people");
+    }
+
+    getAge() {
+        super.getAge();
+    }
+}
+
+module.exports = Car;
